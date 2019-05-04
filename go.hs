b13eb994d7643634c0b88ddb937eff71dd4ed016
Tomaz Gomes Mascarenhas <tomgm1502@gmail.com>
2019-05-04 20:24:52 -0300

    Fixed type

--- go.hs
+++ go.hs
@@ -1,4 +1,4 @@
-fastpow :: (Num a, Integral a) => a -> a -> a
+fastpow :: (Num a, Integral a') => a -> a' -> a
 fastpow b 0 = 1
 fastpow b e = if (mod e 2) == 1 then x * x * b else x * x
     where x = fastpow b (div e 2)
